7dee8ad46b9672f7f5d1a2a3c3c0bbb01c0e63b1
CaiY980 <2920127475@qq.com>
2025-11-29 19:34:39 +0800

    修改项目

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GhostClipboard_FILE C:/Users/caiying/Desktop/file/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/GhostClipboard.exe)
-set(__QT_DEPLOY_TARGET_GhostClipboard_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GhostClipboard_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
